565915472b70b13998199e500b6e2230d5d0847d
shivamsrinath <shivamsrinath@gmail.com>
2026-02-06 20:31:47 +0530

    Data Ingestion done

--- demo.py
+++ demo.py
@@ -1,10 +1,3 @@
-from us_visa.logger import logging
-from us_visa.exception import CustomException
-import sys
-
-# logging.info("Welcome to custom logger")
-
-try:
-    a=2/0
-except Exception as e:
-    raise CustomException(e,sys)
+from us_visa.pipline.training_pipeline import TrainPipeline
+obj = TrainPipeline()
+obj.run_pipeline()
